be7f227841d92d7881891d9d6afc8fce91f50352
Leo Developer <107797348+LeoEstaProgramando@users.noreply.github.com>
2022-08-24 02:47:59 -0500

    Update 6.ClasesYObjetos-2.py

--- 6.ClasesYObjetos-2.py
+++ 6.ClasesYObjetos-2.py
@@ -1,10 +1,26 @@
 class Alumno:
-    nombre = None
-    nota = None
 
-    def __init__(self, nombre, nota):
-        print("Nombre   : ", nombre)
-        print("Nota     : ", nota)
-        print("Resultado: Aprobado") if nota > 10 else print("Resultado: Desaprobado")
+    def inicializar(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
 
-alumno1 = Alumno("Juan", 9)
+    def imprimir(self):
+        print("Nombre: ",self.nombre)
+        print("Nota: ",self.nota)
+
+    def resultado(self):
+            if self.nota < 11:
+                print("El alumno ha desaprobado")
+            else:
+                print("El alumno ha aprobado")
+
+alumno1=Alumno()
+alumno2=Alumno()
+
+alumno1.inicializar("Leo", 15)
+alumno2.inicializar("Juan", 8)
+
+alumno1.imprimir()
+alumno1.resultado()
+alumno2.imprimir()
+alumno2.resultado()
